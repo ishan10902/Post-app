7538e0aae8b5f8f1d2820c7ae7fc24dd4246f17b
ishan10902 <ishansugath@gmail.com>
2025-07-15 23:32:59 +0530

    complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg" style={{ backgroundColor: '#003366' }}>
   <div class="container-fluid">
     <a class="navbar-brand" href="#"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,16 +13,16 @@ const Navbar = () => {
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <Link class="nav-link active" aria-current="page" to="/">Add Post</Link>
+          <Link class="nav-link active" aria-current="page" to="/" style={{ color: 'white' }}>Add Post</Link>
         </li>
         <li class="nav-item">
-          <Link class="nav-link" to="/search">Search</Link>
+          <Link class="nav-link" to="/search" style={{ color: 'white' }}>Search</Link>
         </li>
         <li class="nav-item">
-          <Link class="nav-link" to="/delete">Delete</Link>
+          <Link class="nav-link" to="/delete" style={{ color: 'white' }}>Delete</Link>
         </li>
         <li class="nav-item">
-          <Link class="nav-link" to="/view">View All</Link>
+          <Link class="nav-link" to="/view" style={{ color: 'white' }}>View All</Link>
         </li>
       </ul>
     </div>
